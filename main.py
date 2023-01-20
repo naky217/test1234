77b07a3177cbc90dbe508c81c39f9319bf23f05a
naky217 <123132711+naky217@users.noreply.github.com>
2023-01-20 10:43:28 +0900

    test2

---
+++ main.py
@@ -0,0 +1,14 @@
+def on_gesture_tilt_right():
+    led.plot(2, 0)
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
+def on_gesture_tilt_left():
+    led.unplot(3, 0)
+    led.unplot(2, 0)
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_gesture_shake():
+    led.plot(3, 0)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+music.play_melody("D E F G A G B A ", 120)
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-input.onGesture(Gesture.TiltRight, function () {
-    led.plot(1, 0)
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    led.plot(2, 0)
 })
-input.onGesture(Gesture.TiltLeft, function () {
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
     led.unplot(3, 0)
-    led.unplot(1, 0)
+    led.unplot(2, 0)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     led.plot(3, 0)
 })
 music.playMelody("D E F G A G B A ", 120)

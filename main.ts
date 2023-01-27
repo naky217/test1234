cac50a20c40747fe4fe89f6a7bddd1e7b0cbfbfe
naky217 <123132711+naky217@users.noreply.github.com>
2023-01-27 16:02:06 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+input.onGesture(Gesture.TiltRight, function () {
     led.plot(2, 0)
 })
-input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+input.onGesture(Gesture.TiltLeft, function () {
     led.unplot(4, 0)
     led.unplot(2, 0)
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    led.plot(3, 0)
+input.onGesture(Gesture.Shake, function () {
+    led.plot(3, 2)
 })
 for (let index = 0; index < 4; index++) {
     music.playMelody("D E F G A G B A ", 120)

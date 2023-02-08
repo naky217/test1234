6afdcd0128ecc264af628e46064610b4b5312cee
naky217 <123132711+naky217@users.noreply.github.com>
2023-02-08 17:36:48 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def on_gesture_tilt_right():
-    led.plot(2, 0)
+    if True:
+        for index in range(4):
+            led.plot(2, 0)
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
 def on_gesture_tilt_left():
@@ -11,5 +13,5 @@ def on_gesture_shake():
     led.plot(7, 2)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-for index in range(4):
+for index2 in range(4):
     music.play_melody("D E F G A G B A ", 120)
--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
-    led.plot(2, 0)
+    if (true) {
+        for (let index = 0; index < 4; index++) {
+            led.plot(2, 0)
+        }
+    }
+    
 })
 input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
     led.unplot(4, 0)
@@ -8,6 +13,6 @@ input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     led.plot(7, 2)
 })
-for (let index = 0; index < 4; index++) {
+for (let index2 = 0; index2 < 4; index2++) {
     music.playMelody("D E F G A G B A ", 120)
 }
